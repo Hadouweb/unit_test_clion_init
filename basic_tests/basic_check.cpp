ac8c9d2b2f0aa5f4856e12aa4dce245cf36c63dd
Hadouweb <kengo14@hotmail.fr>
2016-11-20 06:30:29 +0100

    ok

--- basic_tests/basic_check.cpp
+++ basic_tests/basic_check.cpp
@@ -4,8 +4,18 @@
 
 #include "gtest/gtest.h"
 
+#if __cplusplus
+extern "C" {
+#endif
+
+#include "libft.h"
+
+#if __cplusplus
+}
+#endif
+
 TEST(basic_check, test_eq) {
-    EXPECT_EQ(1, 1);
+    EXPECT_EQ(ft_strlen("ok"), 1);
 }
 
 TEST(basic_check, test_neq) {
